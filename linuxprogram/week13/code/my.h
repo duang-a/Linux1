0e807654a20af74c97d59762bc45f113091ace87
wu <2394755178@qq.com>
2019-12-03 09:48:04 +0800

    week13 first

---
+++ linuxprogram/week13/code/my.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <wait.h>
+#include <sys/param.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
---
+++ linuxprogram/week13/code/week13-1.c
@@ -0,0 +1,9 @@
+#include "my.h"
+
+int main()
+{
+	printf("test pid [%d]\n",getpid());
+
+	while(1);
+	return 0;
+}
